c2e2a3c9f13b507af1f3d93ccbec91c6ef3183e4
kallosbela <kallosbela@gmail.com>
2023-05-24 11:03:22 +0200

    baseUrl changed

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -1,2 +1,2 @@
-export const baseUrl="http://localhost:8000"
-// export const baseUrl="https://lobster-app-lsn2k.ondigitalocean.app"
+// export const baseUrl="http://localhost:8000"
+export const baseUrl="https://lobster-app-lsn2k.ondigitalocean.app"
